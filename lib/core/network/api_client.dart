07ae6a8efdab1f682cf852fea73beca371c959e8
vargas <vargascahyadi@gmail.com>
2026-06-12 16:38:15 +0700

    feat(network): add auth token helpers in ApiClient
    Tambahkan metode setAuthToken dan clearAuthToken untuk mengatur header Authorization di ApiClient.

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -33,4 +33,12 @@ class ApiClient {
             },
         ));
     }
+
+    void setAuthToken(String token) {
+        _dio.options.headers['Authorization'] = 'Bearer $token';
+    }
+
+    void clearAuthToken() {
+        _dio.options.headers.remove('Authorization');
+    }
 }
